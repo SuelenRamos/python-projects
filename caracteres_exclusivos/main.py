30e9e2e017f5fccd33598cafb5b749fdbcdf1f95
Suelen <105010254+SuelenRamos@users.noreply.github.com>
2022-07-14 20:48:21 -0300

    Add files via upload

--- caracteres_exclusivos/main.py
+++ caracteres_exclusivos/main.py
@@ -1,42 +1,30 @@
-# algoritmo para determinar a excluvisidade de uma string
-
-def exclusive(w):
-    letras = []
-
-    for letra in w:
-        if letra not in letras:
-            letras.append(letra)
-        else:
-            return False
-    return True
-
-def repeated(sr):
-    letras1 = []
-    letras_repetidas = []
-
-    # checagem da existência de letras repetidas
-    for letra in sr:
-        if letra not in letras1:
-            letras1.append(letra)
-
-        elif letra not in letras_repetidas:
-            letras_repetidas.append(letra)
-
-    return letras_repetidas
+# Gerador de números primos
 
 
-word = input('Digite uma string: ')
+def primos(x, y):
+    num = []
 
-result = exclusive(word.lower())
+    for i in range(x, y):
+        if i == 1:
+            continue
+        if i == 2 or i == 3 or i==5 or i==7:
+            num.append(i)
+        if i % 2 != 0:
+            if i % 3 != 0:
+                if i % 5 != 0:
+                    if i % 7 != 0:
+                        num.append(i)
 
-if result == True:
-    print('A string "%s" possui todos os caracteres exclusivos.' %word)
-else:
-    print('A string "%s" não possui todos os caracteres exclusivos.' %word)
-
-    print('As letras repetidas foram: ', repeated(word.lower()) )
+    return num
 
 
+interval1 = int(input('Digite o início do intervalo: '))
+interval2 = int(input('Digite o final do intevalo: '))
+
+result = primos(interval1, interval2)
+
+for item in result:
+    print(item)
 
 
 
